638d0debbce81958d08339ea16c26dca68f9fb2c
imshahbaz21 <imshahbaz21@gmail.com>
2026-08-12 09:47:23 +0530

    lombok fix

--- src/main/java/com/app/oneklikai/model/csv/YahooCandleStick.java
+++ src/main/java/com/app/oneklikai/model/csv/YahooCandleStick.java
@@ -1,8 +1,13 @@
 package com.app.oneklikai.model.csv;
 
+import com.app.oneklikai.model.entity.CandleStick;
+import lombok.Builder;
+
 import java.time.OffsetDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
+@Builder
 public record YahooCandleStick(
     OffsetDateTime timestamp,
     double open,
@@ -13,4 +18,16 @@ public record YahooCandleStick(
 ) {
     public static final DateTimeFormatter FORMATTER = 
             DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ssXXX");
+
+    public CandleStick toEntity(String symbol){
+        return CandleStick.builder()
+                .symbol(symbol)
+                .open(open)
+                .high(high)
+                .low(low)
+                .close(close)
+                .volume(volume)
+                .timestamp(this.timestamp.atZoneSameInstant(ZoneId.of("Asia/Kolkata")).toInstant())
+                .build();
+    }
 }
---
+++ src/main/java/com/app/oneklikai/model/entity/CandleStick.java
@@ -0,0 +1,40 @@
+package com.app.oneklikai.model.entity;
+
+import com.app.oneklikai.model.csv.YahooCandleStick;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.time.Instant;
+import java.time.ZoneId;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "candle_sticks")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CandleStick {
+
+    @MongoId
+    String id;
+    String symbol;
+    Instant timestamp;
+    double open;
+    double high;
+    double low;
+    double close;
+    double volume;
+
+    public YahooCandleStick toYahooCandleStick() {
+        return YahooCandleStick.builder()
+                .open(open)
+                .high(high)
+                .low(low)
+                .close(close)
+                .volume(volume)
+                .timestamp(timestamp.atZone(ZoneId.of("Asia/Kolkata")).toOffsetDateTime())
+                .build();
+    }
+}
